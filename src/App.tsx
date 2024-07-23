b53940abe20e111fc14936b78e2101131251f8d0
Rishab <rbora@ucsd.edu>
2024-07-23 11:58:02 -0700

    removed warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback } from 'react'; // Removed useEffect
 import axios from 'axios';
 import './App.css';
 import './components/styles.css';
