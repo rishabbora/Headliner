0151c1034f96dd562e243dd173875299ea23649f
Rishab <rbora@ucsd.edu>
2024-07-23 17:49:40 -0700

    frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000; // Use environment variable for port
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
